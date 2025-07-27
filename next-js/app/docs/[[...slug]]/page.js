313e830ca31c338ea148028402a23e2da22a7f95
Ayfer Şahin <ayfers547@gmail.com>
2025-07-28 00:30:19 +0300

    docs ve slug yapısı oluşturuldu

---
+++ next-js/app/docs/[[...slug]]/page.js
@@ -0,0 +1,14 @@
+export default async function Docs({ params }) {
+  const { slug } = await params;
+  if (slug && slug.length === 2) {
+    return (
+      <h1>
+        {slug[0]} özelliği için {slug[1]} konseptini görüntülüyorsunuz
+      </h1>
+    );
+  }
+  if (slug && slug.length === 1) {
+    <h1>{slug[0]} özelliği için dokümanları görüntülüyorsunuz</h1>;
+  }
+  return <h1>Docs Sayfası</h1>;
+}
