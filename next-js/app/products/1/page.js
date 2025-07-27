2caf89a319f7ec185a9e9678a9790336bc479f80
Ayfer Şahin <ayfers547@gmail.com>
2025-07-27 17:06:27 +0300

    iç içe statik klasörler oluşturuldu

---
+++ next-js/app/products/1/page.js
@@ -0,0 +1,3 @@
+export default function Urun1() {
+  return <h1>Ürün 1</h1>;
+}
---
+++ next-js/app/products/page.js
@@ -0,0 +1,13 @@
+export default function ProductList() {
+  return (
+    <div>
+      <h1 className="text-3xl text-blue-500">Ürün Listesi</h1>
+      <ul className="text-2xl">
+        <li>Ürün 1</li>
+        <li>Ürün 2</li>
+        <li>Ürün 3</li>
+        <li>Ürün 4</li>
+      </ul>
+    </div>
+  );
+}
