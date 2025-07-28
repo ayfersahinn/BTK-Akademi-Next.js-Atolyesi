4e8d248d06d5cb021110275405bbef64ea1b4d20
Ayfer Şahin <ayfers547@gmail.com>
2025-07-28 09:23:37 +0300

    yorum özel not found dosyası eklendi

---
+++ next-js/app/products/[productID]/reviews/[reviewID]/not-found.js
@@ -0,0 +1,8 @@
+export default function YorumBulunamadı() {
+  return (
+    <div>
+      <h1 className="text-2xl text-red-600">Yorum bulunamadı</h1>
+      <p>Aradığınız inceleme mevcut değil</p>
+    </div>
+  );
+}
