76d0e6444025f7fbfd2ce9773a4553ac63fc6441
Ayfer Şahin <ayfers547@gmail.com>
2025-07-28 09:19:42 +0300

    yorum bulunamadı sayfası eklendi

--- next-js/app/products/[productID]/reviews/[reviewID]/page.js
+++ next-js/app/products/[productID]/reviews/[reviewID]/page.js
@@ -1,9 +1,13 @@
+import { notFound } from "next/navigation";
 export default async function YorumDetay({ params }) {
   const { productID, reviewID } = await params;
+  if (parseInt(reviewID) > 1000) {
+    notFound();
+  }
   return (
     <div>
       <h1>Yorum: {reviewID}</h1>
-      <h1>Product: {productID}</h1>
+      <h1>Ürün: {productID}</h1>
     </div>
   );
 }
