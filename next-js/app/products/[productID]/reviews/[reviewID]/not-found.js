b5358739add0190fe72796156300b222afaad264
Ayfer Şahin <ayfers547@gmail.com>
2025-07-28 09:28:40 +0300

    usePathname hooku kullanıldı

--- next-js/app/products/[productID]/reviews/[reviewID]/not-found.js
+++ next-js/app/products/[productID]/reviews/[reviewID]/not-found.js
@@ -1,8 +1,15 @@
+"use client";
+import { usePathname } from "next/navigation";
 export default function YorumBulunamadı() {
+  const pathname = usePathname();
+  const productId = pathname.split("/")[2];
+  const reviewId = pathname.split("/")[4];
   return (
     <div>
-      <h1 className="text-2xl text-red-600">Yorum bulunamadı</h1>
-      <p>Aradığınız inceleme mevcut değil</p>
+      <h1 className="text-2xl text-red-600">
+        {reviewId} nolu yorum bulunamadı
+      </h1>
+      <p>{productId} için inceleme mevcut değil.</p>
     </div>
   );
 }
